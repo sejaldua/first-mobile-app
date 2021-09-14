12a80d0d63028d761c2f5dacf52d7241184bd645
sejaldua <sejal.dua@tufts.edu>
2021-09-13 23:08:13 -0400

    ignore exercise files

--- App.js
+++ App.js
@@ -1,37 +1,39 @@
-import React from 'react';
-import { StyleSheet, Text, View, ActivityIndicator, ProgressViewIOS, ProgressBarAndroid, Button, Alert, Dimensions, Platform } from 'react-native';
-
-const {height, width} = Dimensions.get("window");
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  const onButtonPress = () => {
-    console.log(`${new Date().toLocaleTimeString()} button press`)
-  };
   return (
-    <View style={{padding: 50}}>
-      <Text>ready...</Text>
-      {Platform.OS === "ios" && <ProgressViewIOS progress={0.8}/>}
-      {Platform.OS === "android" && 
-      <ProgressBarAndroid 
-        progress={0.8}
-        styleAttr="Horizontal"
-        indeterminate={false}
-        color="blue"
-      />}
-      <ActivityIndicator size="large" color="#61DBFB"/>
-      <Button title="click me" onPress={onButtonPress}/>
-      <Text>OS: {Platform.OS}</Text>
-      <Text>Height: {height}</Text>
-      <Text>Width: {width}</Text>
+    <View style={styles.page}>
+      <Text style={styles.text}>red</Text>
+      <Text style={[styles.text, styles.selectedText]}>
+        green
+      </Text>
+      <Text style={styles.text}>blue</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  page: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+    marginTop: 40,
+    backgroundColor: "#DDD"
   },
+  text: {
+    flex: 1,
+    textAlign: "center",
+    fontSize: 22,
+    color: "red",
+    backgroundColor: "yellow",
+    margin: 10,
+    padding: 5
+  },
+  selectedText: {
+    alignSelf: "flex-start",
+    backgroundColor: "red",
+    color: "yellow"
+  }
 });
