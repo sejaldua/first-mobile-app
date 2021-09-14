278d83f6377b576486ce7b07a009f3512a1a00a8
sejaldua <sejal.dua@tufts.edu>
2021-09-13 22:39:04 -0400

    android compatibility

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View, ActivityIndicator, ProgressViewIOS, Button } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator, ProgressViewIOS, ProgressBarAndroid, Button, Alert, Dimensions, Platform } from 'react-native';
+
+const {height, width} = Dimensions.get("window");
 
 export default function App() {
   const onButtonPress = () => {
@@ -8,9 +10,19 @@ export default function App() {
   return (
     <View style={{padding: 50}}>
       <Text>ready...</Text>
-      <ProgressViewIOS progress={0.8}/>
+      {Platform.OS === "ios" && <ProgressViewIOS progress={0.8}/>}
+      {Platform.OS === "android" && 
+      <ProgressBarAndroid 
+        progress={0.8}
+        styleAttr="Horizontal"
+        indeterminate={false}
+        color="blue"
+      />}
       <ActivityIndicator size="large" color="#61DBFB"/>
       <Button title="click me" onPress={onButtonPress}/>
+      <Text>OS: {Platform.OS}</Text>
+      <Text>Height: {height}</Text>
+      <Text>Width: {width}</Text>
     </View>
   );
 }
